56f27dcacaab89cb55fa4a08c2c47725bf28ad2e
Adrian <adrian.tubio@outlook.com>
2021-06-28 21:59:26 +0800

    update db functions

--- Ui_Files/db.py
+++ Ui_Files/db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-def Authenticate(user=None, password=None, priv=0):
+def Authenticate(user, password, priv=0):
     conn = sqlite3.connect("MovieTicketingSystem.db")
     cur = conn.cursor()
     cur.execute(f'SELECT * FROM Users WHERE username = \'{user}\' AND password = \'{password}\' AND privilege = \'{priv}\'')
@@ -8,12 +8,56 @@ def Authenticate(user=None, password=None, priv=0):
     
     if len(rows) != 0:
         return True
-        
+
     # for row in rows:
     #     if user == row[1] and password == row[2]:
     #         return True
 
     conn.close()
+
+def AddBooking(movie, cinema, amount):
+    conn = sqlite3.connect("MovieTicketingSystem.db")
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO Booking VALUES(?,?,?)", (movie,cinema,amount))
+
+    conn.commit()
+    conn.close()
+
+def AddGenre(genre):
+    conn = sqlite3.connect("MovieTicketingSystem.db")
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO Genre VALUES(?)", genre)
+
+    conn.commit()
+    conn.close()
+
+def GetMovieList(cond = ""):
+    conn = sqlite3.connect("MovieTicketingSystem.db")
+    cur = conn.cursor()
+
+    res = cur.execute(f"SELECT * FROM Movie {cond}").fetchall()
+    conn.close()
+
+    return res
+
+def GetCinemaList():
+    conn = sqlite3.connect("MovieTicketingSystem.db")
+    cur = conn.cursor()
+
+    res = cur.execute(f"SELECT * FROM Cinema").fetchall()
+    conn.close()
+    
+    return res
+
+def getScheduleList(movieID, cinemaID):
+    conn = sqlite3.connect("MovieTicketingSystem.db")
+    cur = conn.cursor()
+
+    res = cur.execute(f"SELECT date, time FROM MovieShowsAt WHERE movieID = {movieID} AND cinemaID = {cinemaID}").fetchall()
+
+    conn.close()
+
+    return res
 
 
 
